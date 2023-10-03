6fc6581888d7ca092aebc64c08af8ade522f0d19
odjkdsfdnf <odjkdsfdnf@email.com>
2023-10-03 11:12:08 +0100

    message

--- Investment.py
+++ Investment.py
@@ -1,8 +1,15 @@
 years = 0
-investment = 100
-while investment < 1000:
+investment = int(input("Enter your initial investment."))
+interest = int(input("Enter your interest rate"))
+target = int(input("Enter your target sum"))
+
+interest = 1 + (interest / 100)
+
+print(interest)
+
+while investment < target:
     print(investment)
-    investment *= 1.1 
+    investment *= interest
     years += 1
 
 print(years)
