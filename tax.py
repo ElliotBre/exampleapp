519b6b69576f20e0471ced7ac47255793ab464f0
Elliot Brooks <elliot.brooks@sky.uk>
2023-10-04 11:26:54 +0100

    Elliot Brooks: added further functionality to tax.py and relevent dependencies

--- tax.py
+++ tax.py
@@ -1,9 +1,20 @@
 import taxDependencies
 
-taxAmount = int(input("please input your gross yearly income"))
+flag = False
+while flag == False:
+     while True:
+        try:
+            taxAmount = float(input("please input your gross yearly income"))
+        except ValueError:
+            print("That is not a number, please enter again. ")
+            continue
+        break
+     
+     incomeTax = taxDependencies.getIncomeTax(taxAmount)
+     print(f"Your income tax will be {incomeTax}, leaving your income at {taxAmount - incomeTax}")
 
-incomeTax = taxDependencies.getIncomeTax(taxAmount)
-print(f"Your income tax will be {incomeTax}, leaving your income at {taxAmount - incomeTax}")
+     if(input("Would you like to calculate again? y/n").lower() != "y"):
+        flag = True
 
 
         
--- taxDependencies.py
+++ taxDependencies.py
@@ -11,5 +11,12 @@ def getIncomeTax(taxAmount):
                 percentage = 0.4
             else:
                 percentage = 0.45
-    taxAmount *= percentage
+    match percentage:
+        case 0.2:
+            taxAmount = (taxAmount - 11850) * percentage
+        case 0.4:
+            taxAmount = (34500 * 0.2) + ((taxAmount - (34500 + 11850)) * 0.4)
+        case 0.45:
+            taxAmount = taxAmount - ((345000 * 0.2) + (150000 * 0.4) + ((taxAmount - (11850 + 34500 + 150000)) * 0.45))
+            
     return taxAmount
