f8fd904476fe5d255f25afb1d3d9a560a35ee0ae
Elliot Brooks <elliot.brooks@sky.uk>
2023-10-05 12:29:13 +0100

    help

--- OOP.py
+++ OOP.py
@@ -3,5 +3,4 @@ import oopDependencies
 owl = oopDependencies.owl
 dodo = oopDependencies.dodo
 
-print(f"here we have two bird species the dodo: {repr(owl)} and the owl: {repr(dodo)}")
-
+print(f"here we have two bird species the dodo: {owl} and the owl: {dodo}")
